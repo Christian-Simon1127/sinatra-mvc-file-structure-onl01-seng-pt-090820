e347237807193aef9a2b8d388f2e3ca0de8ac702
Christian Simon <csimon1127@gmail.com>
2020-12-09 22:37:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,9 @@ class Dog
   @@DOGS = []
   
   def initialize(name, breed, age)
-    @name, @breed, @age = name, breed, age
+    @name = name 
+    @breed = breed 
+    @age = age
     @@DOGS << self
   end
   
